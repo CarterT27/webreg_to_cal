20565d3aa9018a5464dc1c74c52a6802ed3459ed
Carter Tran <carter.tran@gmail.com>
2023-11-17 01:17:17 -0800

    Revert "changed to pdf"
    This reverts commit 937b890a2836170ee519a57c922fb209adbd14d1.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,10 @@ def main():
     st.title("Webreg to Cal")
     "Export your webreg schedule to your favorite calendar app in seconds!"
 
+    st.warning(
+        "Currently the Safari web browser isn't supported. Please use Google Chrome or Firefox to save your webreg schedule as an .html file."
+    )
+
     if st.button("How to Use"):
         st.session_state["show_instructions"] = not st.session_state[
             "show_instructions"
@@ -19,10 +23,11 @@ def main():
             "1. Visit https://act.ucsd.edu/webreg2/start and once you are logged in, select the term that you want to add to your calendar."
         )
         st.write(
-            '2. Once you are at the schedule page, click "Print Schedule" and save the .pdf file.'
+            '2. Once you are at the schedule page, right click the page and click "Save As", "Save Page As...", or something similar.',
+            "The default filename should be webregMain.html",
         )
         st.write(
-            "3. Upload the .pdf file that you just saved back to this website, and it will automatically generate calendar events that you can import to your [Google Calendar](https://support.google.com/calendar/answer/37118?hl=en&co=GENIE.Platform=Desktop) (or another calendar app that supports .csv file imports)."
+            "3. Upload the .html file that you just saved back to this website, and it will automatically generate calendar events that you can import to your [Google Calendar](https://support.google.com/calendar/answer/37118?hl=en&co=GENIE.Platform=Desktop) (or another calendar app that supports .csv file imports)."
         )
 
     included_statuses = st.multiselect(
@@ -35,32 +40,30 @@ def main():
     )
 
     uploaded_file = st.file_uploader(
-        "Upload your webreg schedule in .pdf format", type="pdf"
+        "Upload your webreg schedule in .html format", type="html"
     )
     if uploaded_file is not None:
-        if included_statuses == []:
-            st.error("Warning: No course statuses selected. Only quarter breaks will be added to your calendar.")
-        # try:
-        with st.spinner("Processing..."):
-            cal_csv = webreg_to_cal.main(uploaded_file, included_statuses=included_statuses)
+        try:
+            with st.spinner("Processing..."):
+                cal_csv = webreg_to_cal.main(uploaded_file, included_statuses=included_statuses)
 
-        st.success(
-            "Successfully converted schedule to .csv. Click below to download file."
-        )
+            st.success(
+                "Successfully converted schedule to .csv. Click below to download file."
+            )
 
-        st.download_button(
-            label="Download Webreg CSV",
-            data=cal_csv,
-            file_name="Webreg_Schedule.csv",
-            mime="text/csv",
-        )
-        # except Exception as e:
-        #     # raise (e)
-        #     print(e)
-        #     st.error(
-        #         "Error: Could not process file. Please ensure you are uploading the correct file type. "
-        #         "For more information, please consult the How to Use section."
-        #     )
+            st.download_button(
+                label="Download Webreg CSV",
+                data=cal_csv,
+                file_name="Webreg_Schedule.csv",
+                mime="text/csv",
+            )
+        except Exception as e:
+            # raise (e)
+            print(e)
+            st.error(
+                "Error: Could not process file. Please ensure you are uploading the correct file type. "
+                "For more information, please consult the How to Use section."
+            )
 
     st.write("Questions/Suggestions: https://github.com/CarterT27/webreg_to_cal/issues")
     "Made by Carter Tran"
